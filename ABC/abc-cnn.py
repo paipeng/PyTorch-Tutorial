b146299177f572396ad35a1deb9af62b4bb5ee6d
Pai Peng <p.peng@s2icode.com>
2019-11-28 18:02:16 +0800

    add test

---
+++ ABC/abc-cnn.py
@@ -0,0 +1,136 @@
+"""
+View more, visit my tutorial page: https://morvanzhou.github.io/tutorials/
+My Youtube Channel: https://www.youtube.com/user/MorvanZhou
+
+Dependencies:
+torch: 0.4
+torchvision
+matplotlib
+"""
+# library
+# standard library
+import os
+
+# third-party library
+import torch
+import torch.nn as nn
+import torch.utils.data as Data
+import torchvision
+import matplotlib.pyplot as plt
+from torch.autograd import Variable
+
+from torchvision import transforms
+# torch.manual_seed(1)    # reproducible
+
+# Hyper Parameters
+EPOCH = 1               # train the training data n times, to save time, we just train 1 epoch
+BATCH_SIZE = 50
+LR = 0.001              # learning rate
+DOWNLOAD_MNIST = True
+
+EPOCHS = 2
+BATCH_SIZE = 10
+LEARNING_RATE = 0.003
+TRAIN_DATA_PATH = "./images/"
+TEST_DATA_PATH = "./images/"
+TRANSFORM_IMG = transforms.Compose([
+    #transforms.Resize(640),
+    #transforms.CenterCrop(256),
+    transforms.ToTensor(),
+    #transforms.Normalize(mean=[0.485, 0.456, 0.406],
+    #                     std=[0.229, 0.224, 0.225] )
+])
+
+
+train_data = torchvision.datasets.ImageFolder(root=TRAIN_DATA_PATH, transform=TRANSFORM_IMG)
+train_data_loader = Data.DataLoader(train_data, batch_size=BATCH_SIZE, shuffle=True,  num_workers=4)
+test_data = torchvision.datasets.ImageFolder(root=TEST_DATA_PATH, transform=TRANSFORM_IMG)
+test_data_loader  = Data.DataLoader(test_data, batch_size=BATCH_SIZE, shuffle=True, num_workers=4)
+
+
+
+
+print("Number of train samples: ", len(train_data))
+print("Number of test samples: ", len(test_data))
+print("Detected Classes are: ", train_data.class_to_idx) # classes are detected by folder structure
+
+
+
+
+class CNN(nn.Module):
+    def __init__(self):
+        super(CNN, self).__init__()
+        self.conv1 = nn.Sequential(         # input shape (1, 28, 28)
+            nn.Conv2d(
+                in_channels=3,              # input height
+                out_channels=16,            # n_filters
+                kernel_size=5,              # filter size
+                stride=1,                   # filter movement/step
+                padding=2,                  # if want same width and length of this image after Conv2d, padding=(kernel_size-1)/2 if stride=1
+            ),                              # output shape (16, 28, 28)
+            nn.ReLU(),                      # activation
+            nn.MaxPool2d(kernel_size=2),    # choose max value in 2x2 area, output shape (16, 14, 14)
+        )
+        self.conv2 = nn.Sequential(         # input shape (16, 14, 14)
+            nn.Conv2d(16, 32, 5, 1, 2),     # output shape (32, 14, 14)
+            nn.ReLU(),                      # activation
+            nn.MaxPool2d(2),                # output shape (32, 7, 7)
+        )
+        self.out = nn.Linear(32 * 160 * 160, 2)   # fully connected layer, output 10 classes
+
+    def forward(self, x):
+        x = self.conv1(x)
+        x = self.conv2(x)
+        x = x.view(x.size(0), -1)           # flatten the output of conv2 to (batch_size, 32 * 7 * 7)
+        output = self.out(x)
+        return output, x    # return x for visualization
+
+
+cnn = CNN()
+print(cnn)  # net architecture
+
+optimizer = torch.optim.Adam(cnn.parameters(), lr=LR)   # optimize all cnn parameters
+loss_func = nn.CrossEntropyLoss()                       # the target label is not one-hotted
+
+# following function (plot_with_labels) is for visualization, can be ignored if not interested
+from matplotlib import cm
+try: from sklearn.manifold import TSNE; HAS_SK = True
+except: HAS_SK = False; print('Please install sklearn for layer visualization')
+def plot_with_labels(lowDWeights, labels):
+    plt.cla()
+    X, Y = lowDWeights[:, 0], lowDWeights[:, 1]
+    for x, y, s in zip(X, Y, labels):
+        c = cm.rainbow(int(255 * s / 9)); plt.text(x, y, s, backgroundcolor=c, fontsize=9)
+    plt.xlim(X.min(), X.max()); plt.ylim(Y.min(), Y.max()); plt.title('Visualize last layer'); plt.show(); plt.pause(0.01)
+
+plt.ion()
+# training and testing
+for epoch in range(EPOCH):
+    for step, (x, y) in enumerate(train_data_loader):   # gives batch data, normalize x when iterate train_loader
+        b_x = Variable(x)   # batch x (image)
+        b_y = Variable(y)   # batch y (target)
+        output = cnn(b_x)[0]               # cnn output
+        loss = loss_func(output, b_y)   # cross entropy loss
+        optimizer.zero_grad()           # clear gradients for this training step
+        loss.backward()                 # backpropagation, compute gradients
+        optimizer.step()                # apply gradients
+
+        if step % 50 == 0:
+            test_output, last_layer = cnn(b_x)
+            pred_y = torch.max(test_output, 1)[1].data.numpy()
+            accuracy = float((pred_y == b_y.data.numpy()).astype(int).sum()) / float(b_y.size(0))
+            print('Epoch: ', epoch, '| train loss: %.4f' % loss.data.numpy(), '| test accuracy: %.2f' % accuracy)
+            if HAS_SK:
+                # Visualization of trained flatten layer (T-SNE)
+                tsne = TSNE(perplexity=30, n_components=2, init='pca', n_iter=5000)
+                plot_only = 500
+                low_dim_embs = tsne.fit_transform(last_layer.data.numpy()[:plot_only, :])
+                labels = b_y.numpy()[:plot_only]
+                plot_with_labels(low_dim_embs, labels)
+plt.ioff()
+
+# print 10 predictions from test data
+# test_output, _ = cnn(test_x[:10])
+# pred_y = torch.max(test_output, 1)[1].data.numpy()
+# print(pred_y, 'prediction number')
+# print(test_y[:10].numpy(), 'real number')
